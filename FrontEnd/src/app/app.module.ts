8cd884729b570bf82196e4a9017ac41f7479a20c
martin.ropke <martin.ropke@gmail.com>
2022-05-27 21:28:36 -0300

    Sort and Filter of hospital table

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { HospitalTableComponent } from './hospital-table/hospital-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
