1e4c87eb74041483fb54e582bf72f32c81a8a5a9
raphazik4 <rapha.marchetti@gmail.com>
2022-12-07 20:53:53 -0300

    Update FunctionalitiesGetEmptiestHospitals.js

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetEmptiestHospitals.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetEmptiestHospitals.js
@@ -1,12 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from "axios";
 
-export default class FunctionalitiesGetEmptiestHospitals extends React.Component{
-	render(){
-		return(
-			<div>
-				<button className="btn btn-outline-primary w-100 mt-2">GetEmptiestHospitals</button>
-			</div>
-		)
-	}
+const FunctionalitiesGetEmptiestHospitals = () => {
+	
+	const baseUrl = "http://localhost:30000/get_Emptiest_Hospitals"
+
+	const handleSubmit = event => {
+		axios.get(baseUrl)
+		.then(res => alert(JSON.stringify(res.data)))
+		.catch(err => alert(JSON.stringify(err)))
+	};
+
+	return (
+		<div>
+			<button className="btn btn-outline-primary w-100 mt-2" onClick={handleSubmit} type='submit'>Get Emptiest hospital</button>
+		</div>
+	)
 }
+
+export default FunctionalitiesGetEmptiestHospitals
