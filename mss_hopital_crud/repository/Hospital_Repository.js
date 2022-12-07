e0df15b04e9fa3ba959407cc8e3c0d5349fd2c37
raphazik4 <rapha.marchetti@gmail.com>
2022-12-07 18:41:19 -0300

    Updated mss for mock utilization istead of real database

--- mss_hopital_crud/repository/Hospital_Repository.js
+++ mss_hopital_crud/repository/Hospital_Repository.js
@@ -1,13 +1,13 @@
 const MongoDB = require('../Database/MongoDB')
-const { URL_MONGODB } = require('../GlobalVariables')
-//const { MongoDBMock } = require('../Database/MongoDBMock')
+//const { URL_MONGODB } = require('../GlobalVariables')
+const { MongoDBMock } = require('../Database/MongoDBMock')
 
 
 class Hospital_Repository {
     database
     
     constructor() {
-        this.database = new MongoDB(URL_MONGODB) //new MongoDBMock()
+        this.database = new MongoDBMock() // new MongoDB(URL_MONGODB)
     }
 
     add_Hospital(hospital) {
