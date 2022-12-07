2e9e57d415e18e3531ce1c3452ef18d974a786f0
raphazik4 <rapha.marchetti@gmail.com>
2022-12-07 20:53:49 -0300

    Update FunctionalitiesGetClosestAndEmptiestHospital.js

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetClosestAndEmptiestHospital.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetClosestAndEmptiestHospital.js
@@ -1,12 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from "axios";
 
-export default class FunctionalitiesGetClosestAndEmptiestHospital extends React.Component{
-	render(){
-		return(
-			<div>
-				<button className="btn btn-outline-primary w-100 mt-2">GetClosestAndEmptiestHospital</button>
-			</div>
-		)
-	}
+const FunctionalitiesGetClosestAndEmptiestHospital = () => {
+	
+	const baseUrl = "http://localhost:30000/closest_And_Emptiest_Hospitals"
+
+	const handleSubmit = event => {
+		axios.get(baseUrl)
+		.then(res => alert(JSON.stringify(res.data)))
+		.catch(err => alert(JSON.stringify(err)))
+	};
+
+	return (
+		<div>
+			<button className="btn btn-outline-primary w-100 mt-2" onClick={handleSubmit} type='submit'>Get Closest and Emptiest hospital</button>
+		</div>
+	)
 }
+
+export default FunctionalitiesGetClosestAndEmptiestHospital
