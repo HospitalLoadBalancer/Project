88b8af24af63c3ecf7b87e0c7da49d8a689e477c
raphazik4 <rapha.marchetti@gmail.com>
2022-12-07 19:57:06 -0300

    Built base for Functionalitie

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesAddHospital.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesAddHospital.js
@@ -1,12 +1,44 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import FormComponent from "../FormComponent";
 
-export default class FunctionalitiesAddHospital extends React.Component{
-	render(){
-		return(
-			<div>
-				<button className="btn btn-outline-primary w-100 mt-2">AddHospital</button>
-			</div>
-		)
-	}
+const FunctionalitiesAddHospital = () => {
+	let formInputs = [
+		"id", "name", "lat", "lng", "address", "number_of_beds", "occupation"
+	]
+
+	const handleSubmit = event => {
+		let formData = extractFormData(event)
+		const body = {
+				id: formData.id, 
+				name: formData.name,
+				location: { lat: formData.lat, lng: formData.lng},
+				address: formData.address,
+				number_of_beds: formData.number_of_beds,
+				occupation : formData.occupation
+		}
+		console.log(body)
+		// request
+	};
+
+	const extractFormData = (event) => {
+		event.preventDefault();
+		let data = {}
+		for (let i = 0; i < formInputs.length; i++) {
+			data[event.target[i].placeholder] = event.target[i].value
+		}
+		return data
+	};
+
+	return (
+		<div>
+			<h1>AddHospital</h1>
+			<FormComponent
+				formInput={formInputs}
+				formSubmit={handleSubmit}
+			/>
+		</div>
+	)
 }
+
+export default FunctionalitiesAddHospital
