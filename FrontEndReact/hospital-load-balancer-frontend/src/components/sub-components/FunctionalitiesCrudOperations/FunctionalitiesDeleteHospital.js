45e440da112f83dcace5aaf6361abe246624f0c0
raphazik4 <rapha.marchetti@gmail.com>
2022-12-07 20:25:17 -0300

    Update FunctionalitiesDeleteHospital.js

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesDeleteHospital.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesDeleteHospital.js
@@ -1,12 +1,42 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import FormComponent from "../FormComponent";
 
-export default class FunctionalitiesDeleteHospital extends React.Component{
-	render(){
-		return(
-			<div>
-				<button className="btn btn-outline-primary w-100 mt-2">DeleteHospital</button>
-			</div>
-		)
-	}
+const FunctionalitiesDeleteHospital = () => {
+	
+	const baseUrl = "http://localhost:30100/delete_Hospital_By_Id"
+
+	let formInputs = [
+		"id"
+	]
+
+	const handleSubmit = event => {
+		let formData = extractFormData(event)
+		
+		axios.delete(`${baseUrl}?id=${formData.id}`)
+		.then(res => alert(JSON.stringify(res.data)))
+		.catch(err => alert(JSON.stringify(err)))
+	};
+
+	const extractFormData = (event) => {
+		event.preventDefault();
+		let data = {}
+		for (let i = 0; i < formInputs.length; i++) {
+			data[event.target[i].placeholder] = event.target[i].value
+		}
+		return data
+	};
+
+	return (
+		<div>
+			<h1>Delete Hospital by id</h1>
+			<FormComponent
+				formInput={formInputs}
+				formSubmit={handleSubmit}
+			/>
+		</div>
+	)
 }
+
+export default FunctionalitiesDeleteHospital
