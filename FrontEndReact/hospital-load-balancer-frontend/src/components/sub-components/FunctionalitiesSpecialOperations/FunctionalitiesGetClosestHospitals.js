d82d8e42f2959a974546fddf86c66b653d16f5e8
raphazik4 <rapha.marchetti@gmail.com>
2022-12-08 20:51:40 -0300

    Updated components to use the table

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetClosestHospitals.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetClosestHospitals.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import HospitalDataTable from '../HospitalDataTable';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
 
 const FunctionalitiesGetClosestHospitals = () => {
 
 	const baseUrl = "http://localhost:30000/closest_Hospitals"
+	const [hospitals, setHospitals] = useState([]);
 
-	const handleSubmit = event => {
+	const handleSubmit = () => {
 		axios.get(baseUrl)
-		.then(res => alert(JSON.stringify(res.data)))
+		.then(res => setHospitals(res.data))
 		.catch(err => alert(JSON.stringify(err)))
 	};
 
@@ -16,6 +18,8 @@ const FunctionalitiesGetClosestHospitals = () => {
 		<div className="border border-primary m-4 p-2 rounded">
 			<h4 className="text-center">Get Closest hospital</h4>
 			<button className="btn btn-outline-primary w-100 mt-2" onClick={handleSubmit} type='submit'>Get Closest hospital</button>
+			<br /><br />
+			<HospitalDataTable hospitals={hospitals}></HospitalDataTable>
 		</div>
 	)
 }
--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetEmptiestHospitals.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesSpecialOperations/FunctionalitiesGetEmptiestHospitals.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import HospitalDataTable from '../HospitalDataTable';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
 
 const FunctionalitiesGetEmptiestHospitals = () => {
 	
 	const baseUrl = "http://localhost:30000/get_Emptiest_Hospitals"
+	const [hospitals, setHospitals] = useState([]);
 
 	const handleSubmit = event => {
 		axios.get(baseUrl)
-		.then(res => alert(JSON.stringify(res.data)))
+		.then(res => setHospitals(res.data.message))
 		.catch(err => alert(JSON.stringify(err)))
 	};
 
@@ -16,6 +18,8 @@ const FunctionalitiesGetEmptiestHospitals = () => {
 		<div className="border border-primary m-4 p-2 rounded">
 			<h4 className="text-center">Get Emptiest hospital</h4>
 			<button className="btn btn-outline-primary w-100 mt-2" onClick={handleSubmit} type='submit'>Get Emptiest hospital</button>
+			<br /><br />
+			<HospitalDataTable hospitals={hospitals}></HospitalDataTable>
 		</div>
 	)
 }
