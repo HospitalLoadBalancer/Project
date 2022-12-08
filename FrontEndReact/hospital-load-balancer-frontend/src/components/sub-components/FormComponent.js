07ec87fa0a8ebf80ebd52934fccdc9fb723cbf19
raphazik4 <rapha.marchetti@gmail.com>
2022-12-08 20:54:45 -0300

    Update FormComponent.js

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FormComponent.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FormComponent.js
@@ -6,7 +6,7 @@ export default class FormComponent extends React.Component {
         return (
             <div>
                 <form onSubmit={this.props.formSubmit}>
-                    {this.props.formInput.map(item => <input type="text" placeholder={item} />
+                    {this.props.formInput.map(item => <input className="m-2 border rounded border-secondary text-center" type="text" placeholder={item} />
                     )}
                     <button className="btn btn-outline-primary w-100 mt-2">{this.props.formSubmitDescription}</button>
                 </form>
