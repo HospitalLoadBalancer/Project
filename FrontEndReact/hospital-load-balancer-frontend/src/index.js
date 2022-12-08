ef1292f23c65e617a418788a240f3c9380bebd9a
raphazik4 <rapha.marchetti@gmail.com>
2022-12-08 19:14:23 -0300

    Removed unnecessary Search component

--- FrontEndReact/hospital-load-balancer-frontend/src/index.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Functionalities from './components/Functionalities'
-import Search from './components/Search'
 
 export default function App(){
     return(
         <div className="align-items-center m-2">
             <Functionalities></Functionalities>
-            <Search></Search>
         </div>
     )
 }
