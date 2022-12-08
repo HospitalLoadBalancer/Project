b19652c6bdbcb0a336fdce0d6d3f74b70fa5b64d
raphazik4 <rapha.marchetti@gmail.com>
2022-12-08 20:29:07 -0300

    Applied primereact to the page

--- FrontEndReact/hospital-load-balancer-frontend/src/index.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Functionalities from './components/Functionalities'
+import "primereact/resources/themes/fluent-light/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 export default function App(){
     return(
