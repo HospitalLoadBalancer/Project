0086162340cefdefbe252945307d3b26833928e4
raphazik4 <rapha.marchetti@gmail.com>
2022-12-08 20:31:51 -0300

    Added table for the GetAllHospitals response

--- FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesGetAllHospital.js
+++ FrontEndReact/hospital-load-balancer-frontend/src/components/sub-components/FunctionalitiesCrudOperations/FunctionalitiesGetAllHospital.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 
 const FunctionalitiesGetAllHospital = () => {
 	const baseUrl = "http://localhost:30100/get_All_Hospitals"
 
-	const handleSubmit = event => {
+	const [products, setProducts] = useState([]);
+    const columns = [
+        {field: 'id', header: 'Id'},
+        {field: 'name', header: 'Name'},
+        {field: 'location.lat', header: 'Latitude'},
+		{field: 'location.lng', header: 'Longitude'},
+        {field: 'address', header: 'Address'},
+		{field: 'number_of_beds', header: 'Number of Beds'},
+		{field: 'occupation', header: 'Occupation'}
+    ];
+
+    const dynamicColumns = columns.map((col,i) => {
+        return <Column key={col.field} field={col.field} header={col.header} />;
+    });
+
+	const handleSubmit = () => {
 		axios.get(baseUrl)
-		.then(res => alert(JSON.stringify(res.data)))
+		.then(res => setProducts(res.data))
 		.catch(err => alert(JSON.stringify(err)))
 	};
 
@@ -15,6 +32,14 @@ const FunctionalitiesGetAllHospital = () => {
 		<div className="border border-primary m-4 p-2 rounded">
 			<h4 className="text-center">Get All Hospitals</h4>
 			<button className="btn btn-outline-primary w-100 mt-2" onClick={handleSubmit} type='submit'>Get All Hospitals</button>
+			<br /> <br />
+			<div className="card border border-primary rounded">
+				<div className="card">
+					<DataTable value={products} responsiveLayout="scroll">
+						{dynamicColumns}
+					</DataTable>
+				</div>
+            </div>
 		</div>
 	)
 }
